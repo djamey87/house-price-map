450155e82935fe82f9105806ab5a214b6886df74
djamey <djamey87@gmail.com>
2021-02-27 17:31:33 +0000

    MERN bootstrap project

---
+++ frontend/build/precache-manifest.f6c9050c827e2e4d794359fa1f4651a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "324d742163298d207dfc276c6da1decf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17ecb59547b0a0d9034d",
+    "url": "/static/css/main.91ff1020.chunk.css"
+  },
+  {
+    "revision": "0ee1c8e25b9cf622c776",
+    "url": "/static/js/2.9eea6c9c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.9eea6c9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17ecb59547b0a0d9034d",
+    "url": "/static/js/main.a815a359.chunk.js"
+  },
+  {
+    "revision": "4792b66c7ea268d3b997",
+    "url": "/static/js/runtime-main.d43be887.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
